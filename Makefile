5a9881c353e8d103faf495c1498907871b985b7d
Neben <neben@neben-2.home>
2022-03-02 15:36:54 +0100

    .

---
+++ Makefile
@@ -0,0 +1,37 @@
+#MAKEFLAGS += --silent
+
+NAME = ircserv
+CC = c++
+CPPFLAGS = -g3 #-Wextra -Wall -Werror
+INC_DIR = ./
+INC =
+
+SRC_DIR = ./
+SRC =	main.cpp \
+
+OBJ_DIR = .obj/
+OBJ = $(SRC:%.cpp=$(OBJ_DIR)%.o)
+
+.PHONY : all clean fclean re exec
+
+all: $(NAME) exec
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) -I$(INC_DIR) $(OBJ) -o $(NAME)
+
+$(OBJ_DIR)%.o: $(SRC_DIR)%.cpp $(addprefix $(INC_DIR),$(INC))
+	mkdir -p $(@D)
+	$(CC) $(CPPFLAGS) -I$(INC_DIR) -c $< -o $@
+
+exec:
+	./$(NAME)
+
+clean:
+	/bin/rm -rf $(OBJ_DIR)
+
+fclean: clean
+	/bin/rm -f $(NAME)
+
+re: 
+	$(MAKE) fclean
+	$(MAKE) all
---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <poll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <unistd.h>
+
+#define IP_ADDRESS "127.0.0.1"
+
+#define IP_PROTOCOL 0
+#define TCP_PROTOCOL 6
+
+/*
+to create a socket
+Domain -> Protocol available for this domain ->
+
+A socket name in the Internet domain is an Internet address, made up of a 32-bit IP address and a 16-bit port address.
+
+FIONBIO to set sockets I/O as non-blocking
+*/
+
+void    putstr(char *str)
+{
+    while (*str)
+        putchar(*str++);
+}
+
+int main(void)
+{
+    setbuf(stdout, NULL);
+    //struct addrinfo addr;
+    //in_addr_t addr = inet_addr(IP_ADDRESS);
+    protoent *proto = getprotobyname("TCP");
+    if (!proto)
+        return 0;
+
+    /*  socket */
+    int sock = socket(PF_INET, SOCK_STREAM, proto->p_proto);
+
+    /*  bind */
+    struct sockaddr_in addr;
+    socklen_t   length;
+
+    addr.sin_len = sizeof(addr);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(9649);
+    // addr.sin_addr.s_addr = inet_addr("192.168.1.17");
+    addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(addr.sin_zero, sizeof(addr.sin_zero));
+    length = sizeof(addr);
+    bind(sock, (const struct sockaddr *)&addr, length);
+    getsockname(sock, (struct sockaddr *)&addr, &length);
+    printf("addr=%s | port=%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+
+    /* listen */
+    listen(sock, 1);
+
+    /* accept */
+    socklen_t   addr_len = sizeof(addr);
+    int sock2, rval, pw, welcome = 0;
+    char buf[1025];
+
+    
+    while (1)
+    {
+        if ((sock2 = accept(sock, (struct sockaddr *)&addr, &addr_len)) == -1)
+            break ;
+        printf("connexion réussie ! sock2=%d\n", sock2);
+        while ((rval = read(sock2, buf, 1024)))
+        {
+            buf[1024] = 0;
+            //write(descriptor, buf, 1024);
+            printf("%s", buf);
+            bzero(buf, 1024);
+        }
+        printf("\n");
+
+        struct sockaddr_in addr2;
+        socklen_t   addr2_len = sizeof(addr2);
+
+		getsockname(sock2, (struct sockaddr *)&addr2, &addr2_len);
+        printf("addr2=%s | port2=%d <-\n", inet_ntoa(addr2.sin_addr), ntohs(addr2.sin_port));
+        //connect(sock2, )
+        close(sock2);
+    }
+
+    /*  connect 
+    printf("%d\n", connect(sock, &address, socklen));
+*/
+
+    return 0;
+}
