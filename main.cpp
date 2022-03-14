1445ed1facfb10fa29b6c99d5febf3a840f35a8d
Sylvain Ducamp <sylducam@gmail.com>
2022-03-14 11:09:17 +0100

    comments + limechat

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 #include <sys/errno.h>
 #include <unistd.h>
 
-#define ERROR 1
+#define ERROR 1 // je trouve -1 plus logique perso
 #define PROTO "TCP"
 #define PORT 16385
 #define SOCK_DOMAIN AF_INET
@@ -33,7 +33,8 @@ int	create_server_descriptor(void)
     protoent *proto;
 	struct sockaddr_in server_addr;
 
-    // on récupère l'index du protocole TCP dans /etc/protocols (sous UNIX seulement)
+    // on récupère l'index du protocole TCP dans /etc/protocols
+	// (sous UNIX seulement)
     if ((proto = getprotobyname(PROTO)) == NULL)
     {
         std::cout << "Couldn't find protocol: " << PROTO << std::endl;
@@ -52,12 +53,18 @@ int	create_server_descriptor(void)
 	server_addr.sin_port = htons(PORT);
 	inet_pton(SOCK_DOMAIN, IP_ADDR, &server_addr.sin_addr);
 
-    //setsockopt SO_REUSEADDR permet de réutiliser des adresses déjà utilisées (ça permet de fix le fait que parfois les fd des sockets ne sont pas tout le temps complètements supprimés, du coup ça met une erreur au moment du bind())
+    // setsockopt SO_REUSEADDR permet de réutiliser des adresses déjà utilisées
+	// (ça permet de fix le fait que parfois les fd des sockets ne sont pas tout
+	// le temps complètements supprimés, du coup ça met une erreur au moment du
+	// bind())
     sockopt_reuseaddr_val = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &sockopt_reuseaddr_val, sizeof(sockopt_reuseaddr_val));
 
     // on bind l'adresse du serveur au socket
     if (bind(server_fd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+	// faire cast c++ | il faudra unlink selon le man "Binding a name in the
+	// UNIX domain creates a socket in the file system that must be deleted
+	// by the caller when it is no longer needed (using unlink(2))."
     {
         std::cout << "Server socket error " << errno << " -> bind() : " << strerror(errno) << std::endl;
         return (ERROR);
@@ -70,7 +77,10 @@ int	create_server_descriptor(void)
         return (ERROR);
     }
 
-    //on set le server_fd en O_NONBLOCK pour que accept ne loop pas en attendant une connexion et on surveille les I/O des sockets avec poll en mettant le timeout à -1 pour que ça soit lui qui attende indéfiniment une connexion
+    // on set le server_fd en O_NONBLOCK pour que accept ne loop pas en
+	// attendant une connexion et on surveille les I/O des sockets avec poll en
+	// mettant le timeout à -1 pour que ça soit lui qui attende indéfiniment une
+	// connexion
     if (fcntl(server_fd, F_SETFL, O_NONBLOCK) == -1)
     {
         std::cout << "Server socket error " << errno << " -> fcntl(*, F_SETFL, O_NONBLOCK) : " << strerror(errno) << std::endl;
@@ -163,7 +173,9 @@ int main(void)
 	char	recv_buf[RECV_BUF_SIZE + 1];
 	ssize_t	recv_length;
 
-    setbuf(stdout, NULL); // debug, pour éviter que printf print les lignes que quand y'a un /n, à la place il print à chaque caractère
+    setbuf(stdout, NULL); // debug, pour éviter que printf print les lignes que
+						  // quand y'a un \n, à la place il print à chaque
+						  // caractère
 	server_fd = create_server_descriptor();
 
     //on ajoute server_fd au tableau pollfd requis pour poll
@@ -183,7 +195,8 @@ int main(void)
 				// 0 étant l'index dans le tableau pfds pour server_fd
 				if (i == 0)
 				{
-                    // fcntl O_NONBLOCK du coup la fonction se bloque pas si elle a pas de nouvelles connexions
+                    // fcntl O_NONBLOCK du coup la fonction se bloque pas si
+					// elle a pas de nouvelles connexions
 					if ((client_fd = accept(server_fd, NULL, 0)) == -1)
 					{
 						std::cout << "Client socket error " << errno << " -> accept() : " << strerror(errno) << std::endl;
@@ -205,7 +218,8 @@ int main(void)
 					{
 						recv_buf[recv_length] = 0;
 						parse_client_packets(client_fd, recv_buf);
-						//send_msg_to_all_fds(pfds, recv_buf, recv_length, nb_pfds, pfds[i].fd);
+						// send_msg_to_all_fds(pfds, recv_buf, recv_length,
+						// nb_pfds, pfds[i].fd);
 					}
 				}
 			}
