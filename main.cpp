1d6b01e49d29386657c76c167fdf7da5dc287e71
Neben <neben@macben.local>
2022-03-16 15:31:27 +0100

    Version qui marche avec ClientsMonitoringList, et parsing_packet qui execute bien les bonnes commandes

--- main.cpp
+++ main.cpp
@@ -91,7 +91,6 @@ int	create_server_descriptor(void)
 	return (server_fd);
 }
 
-/*
 std::vector<std::string> string_split(std::string s, const char delimiter)
 {
     size_t start=0;
@@ -153,34 +152,35 @@ void	(*g_commands_functions[NB_COMMANDS])(std::vector<std::string>) = {
 	nick_command
 };
 
-void	parse_client_packet(ClientsMonitoringList &Client, std::string packet)
+void	parse_client_packet(ClientsMonitoringList &Client, int client_fd, std::string packet)
 {
 	int	command_index;
+	std::string current_command;
+	int	newline_pos;
 
-	if (packet.find('\n') != std::string::npos)
+	Client.current_packet += packet;
+	while ((newline_pos = Client.current_packet.find('\n')) != std::string::npos)
 	{
-		packet.push_back('\n');
-		packet.push_back('\r');
-		std::cout << "A" << std::endl;
-		std::vector<std::string> split_packet = string_split(packet, ' ');
-		if (split_packet.size() > 0)
-		{
-			std::cout << "B " << packet << std::endl;
+		current_command = Client.current_packet.substr(0, newline_pos);
+		//current_command.erase(current_command.find('\r'));
+		//std::cout << "(" << Client.current_packet << ")" << std::endl;
+		Client.current_packet.erase(0, newline_pos + 1);
+		std::vector<std::string> split_packet = string_split(current_command, ' ');
+		// Je suis pas sûr qu'on ait besoin de check ça
+		// if (split_packet.size() > 0)
+		// {
 			if ((command_index = get_command_index(split_packet[0])) >= 0)
 				g_commands_functions[command_index](split_packet);
 			else
-				send(Client.fd, "Error: unknown command", strlen("Error: unknown command"), 0);
-		}
-		else
-			send(Client.fd, "Error: no input command", strlen("Error: no input command"), 0);
+				send(client_fd, "Error: unknown command\n", strlen("Error: unknown command\n"), 0);
+		// }
+		// else
+		// 	send(client_fd, "Error: no input command\n", strlen("Error: no input command\n"), 0);
 	}
-	else
-		Client.current_packet += packet;
 }
-		*/
 
 //overwrite sur un descriptor qui a ete remove (mis a -1)
-void    add_descriptor_to_poll(int fd, struct pollfd *pfds, nfds_t &nb_pfds)
+void    add_descriptor_to_poll(int fd, ClientsMonitoringList *Clients, struct pollfd *pfds, nfds_t &nb_pfds)
 {
 	nfds_t i = 0;
 
@@ -191,6 +191,8 @@ void    add_descriptor_to_poll(int fd, struct pollfd *pfds, nfds_t &nb_pfds)
 		i++;
 	}
 	std::cout << "Added fd=" << fd << " to pfds[" << i << "] with an actual nb_pfds=" << nb_pfds + 1 << std::endl;
+	if (Clients)
+		bzero(&Clients[i - 1], sizeof(ClientsMonitoringList));
     pfds[i].fd = fd;
     pfds[i].events = POLLIN;
     nb_pfds++;
@@ -229,14 +231,14 @@ void	print_pfds(struct pollfd *pfds, nfds_t npfds)
 int	monitor_clients(int server_fd)
 {
 	struct pollfd pfds[MAX_CLIENT_CONNEXIONS];
-	ClientsMonitoringList Clients[MAX_CLIENT_CONNEXIONS];
+	ClientsMonitoringList Clients[MAX_CLIENT_CONNEXIONS - 1];
 	nfds_t nb_clients = 0;
     int nb_ready_clients, client_fd;
 	char	recv_buf[RECV_BUF_SIZE + 1];
 	ssize_t	recv_length;
 
     //on ajoute server_fd au tableau pollfd requis pour poll
-	add_descriptor_to_poll(server_fd, pfds, nb_clients);
+	add_descriptor_to_poll(server_fd, NULL, pfds, nb_clients);
 	print_pfds(pfds, nb_clients);
 	while (1)
 	{
@@ -258,7 +260,7 @@ int	monitor_clients(int server_fd)
 						std::cout << "Client monitoring error " << errno << " -> accept() : " << strerror(errno) << std::endl;
 						break ;
 					}
-					add_descriptor_to_poll(client_fd, pfds, nb_clients);
+					add_descriptor_to_poll(client_fd, Clients, pfds, nb_clients);
 					print_pfds(pfds, nb_clients);
 				}
 				else
@@ -285,10 +287,8 @@ int	monitor_clients(int server_fd)
 					//on a reçu un paquet! on l'ouvre :-)
 					else 
 					{
-						std::cout << "RECEIVED" << std::endl;
 						recv_buf[recv_length] = 0;
-						// parse_client_packet(pfds[i], recv_buf);
-						// send_msg_to_all_fds(pfds, recv_buf, recv_length, nb_pfds, pfds[i].fd);
+						parse_client_packet(Clients[i - 1], client_fd, recv_buf);
 					}
 				}
 			}
